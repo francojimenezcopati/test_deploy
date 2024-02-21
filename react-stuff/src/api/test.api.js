cdc88086bb238e60868608df3a7617690060dd64
Francojimenezcopati <fjcopati@gmail.com>
2024-02-21 12:00:52 -0300

    api url fix

--- react-stuff/src/api/test.api.js
+++ react-stuff/src/api/test.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const TESTS_API_URL = "http://127.0.0.1:8000/api/v1/tests/";
-// const TESTS_API_URL = "https://crud-django-react.up.railway.app/api/v1/tests/";
+// const TESTS_API_URL = "http://127.0.0.1:8000/api/v1/tests/";
+const TESTS_API_URL = "https://testdeploy.up.railway.app//api/v1/tests/";
 
 const testsApi = axios.create({
     baseURL: TESTS_API_URL,
